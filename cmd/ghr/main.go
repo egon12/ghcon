035e8803ebccaa0d16a764bed8e2367b1de0f39f
Egon Firman <egon.firman@tokopedia.com>
2020-04-11 05:37:20 +0700

    adding cancel and finish

--- cmd/ghr/main.go
+++ cmd/ghr/main.go
@@ -16,6 +16,16 @@ const (
 )
 
 func main() {
+	if os.Args[1] == "cancel" {
+		cancel()
+		return
+	}
+
+	if os.Args[1] == "finish" {
+		finish()
+		return
+	}
+
 	comment := os.Args[2]
 
 	filePath, line, err := path.ParseFileAndLine(os.Args[1])
@@ -35,4 +45,32 @@ func main() {
 
 	r.AddComment(filePath, line, comment)
 
+}
+
+func finish() {
+	comment := os.Args[2]
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv(GithubToken)})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	clientV4 := githubv4.NewClient(tc)
+
+	cs := commit.NewSource(clientV4)
+	c := cs.GetCurrentCommit()
+
+	r := review.NewProcess(clientV4)
+	r.Start(c)
+	r.Finish(comment)
+}
+
+func cancel() {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv(GithubToken)})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	clientV4 := githubv4.NewClient(tc)
+
+	cs := commit.NewSource(clientV4)
+	c := cs.GetCurrentCommit()
+
+	r := review.NewProcess(clientV4)
+	r.Start(c)
+	r.Cancel()
 }

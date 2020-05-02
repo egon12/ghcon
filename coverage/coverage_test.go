3afae81d21f579e77f4e3a126d507c4cc9994d25
Egon Firman <egon.firman@gmail.com>
2020-05-03 03:28:53 +0700

    fix NotInCoverage test

--- coverage/coverage_test.go
+++ coverage/coverage_test.go
@@ -2,14 +2,11 @@ package coverage
 
 import (
 	"testing"
-
-	"github.com/egon12/ghr/path"
 )
 
 func TestGoCoverageInGit_NotInCoverage(t *testing.T) {
 	c, _ := FromProfile("cover.out")
-	filepath, _ := path.GetFileWithPackagePath("coverage.go")
-	ranges := c.NotInCoverageLines(filepath)
+	ranges := c.NotInCoverageLines("coverage.go")
 
 	if len(ranges) == 0 {
 		t.Error("It should not zero")
